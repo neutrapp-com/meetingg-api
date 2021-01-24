ed1579f36a66bad835158ea79d2951d01a0299bb
yassinrais <yassmailrais@gmail.com>
2021-01-24 19:54:20 +0100

    Update Create First Components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="listbutton">
+    <square-button icon="videocam" color="#ff742e" title="New Meeting" description="Setup new meeting" />
+    <square-button icon="add-circle" color="#0e78f9" title="Join Meeting" description="Via invitation link" />
+    <square-button icon="calendar-outline" color="#0e78f9" title="Schedule" description="Plan your meetings" />
+    <square-button icon="desktop-outline" color="#0e78f9" title="Share  Screen" description="Show your work" />
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'vue-ionicons/ionicons.scss';
+
+import SquareButton from './components/partials/cards/SquareButton.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { SquareButton },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/css/index.css"></style>
+
+
+<style lang="scss">
+  .listbutton{
+    display: flex;
+
+    > div {
+      margin-right: 100px;
+    }
+  }
 </style>
