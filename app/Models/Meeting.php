de4fe168dc0a18f3f18e9b24786037c6df86194d
ducl0006 <remy.duclos@etudiant.univ-reims.fr>
2021-03-16 16:31:09 +0100

    🧜‍♂️ Meeting Controller

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -103,4 +103,60 @@ class Meeting extends BaseModel
 
         $this->hasMany('id', 'Meetingg\Models\Meeting\User', 'meeting_id', ['alias' => 'MeetingUsers']);
     }
+
+
+    /**
+     * Get Meeting Profile
+     *
+     * @return array
+     */
+    public function getProfile() : array
+    {
+        $profile = [
+            'users' => []
+        ];
+
+        /**
+         * Users toArray
+         */
+        foreach ($this->Users ?: [] as $user) {
+            array_push($profile['users'], $user->getProfile([], ['id','firstname','lastname','avatar'], true));
+        }
+        
+        /**
+         * Self toArray
+         */
+        foreach (['id', 'title' ,'avatar' , 'color','created_at','updated_at'] as $key) {
+            $profile[$key] = $this->$key;
+        }
+
+        return $profile;
+    }
+
+ /**
+     * Get Meeting & User
+     *
+     * @param string $meetingId
+     * @param string $userId
+     * @return self|null
+     */
+    public static function userMeeting(string $meetingId, string $userId) :? self
+    {
+        if (false === self::validUUID($meetingId)) {
+            return null;
+        }
+
+        return self::query()
+            ->join(MeetingUser::class, "du.meeting_id = ".self::class.".id AND du.user_id = :user_id:", "du")
+            ->andWhere(self::class.'.id = :meeting_id:')
+            ->bind(
+                [
+                    'user_id' => $userId,
+                    'meeting_id' => $meetingId
+                ]
+            )
+            ->execute()
+            ->getFirst();
+    }
+
 }
